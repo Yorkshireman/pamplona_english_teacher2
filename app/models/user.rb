a917d7c4baf090a4780204dff3312bfe1829f984
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-02-13 18:21:04 +0100

    add name to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	authenticates_with_sorcery!
 
+	validates :name, presence: true
 	validates :password, length: { minimum: 4 }
 	validates :password, confirmation: true
 	validates :password_confirmation, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213165722) do
+ActiveRecord::Schema.define(version: 20150213170731) do
 
   create_table "reviews", force: :cascade do |t|
     t.string   "testimonials"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20150213165722) do
     t.string   "testimonial"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
