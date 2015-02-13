e320289fbe3356e56ff33b566b45d4b1a494eeaf
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-02-13 17:48:58 +0100

    generate working user authentication with email and password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212121854) do
+ActiveRecord::Schema.define(version: 20150213154706) do
 
   create_table "reviews", force: :cascade do |t|
     t.string   "testimonials"
