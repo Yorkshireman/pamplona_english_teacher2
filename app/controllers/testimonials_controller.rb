1bcd82815004eabb390fc41dbf9e04112de298a7
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-02-14 01:37:12 +0100

    deleted unnecessary json code. About to generate a new migration to add a user_id to Testimonials before setting up associations

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -1,64 +1,41 @@
 class TestimonialsController < ApplicationController
   before_action :set_testimonial, only: [:show, :edit, :update, :destroy]
 
-  # GET /testimonials
-  # GET /testimonials.json
   def index
     @testimonials = Testimonial.all
   end
 
-  # GET /testimonials/1
-  # GET /testimonials/1.json
   def show
   end
 
-  # GET /testimonials/new
   def new
     @testimonial = Testimonial.new
   end
 
-  # GET /testimonials/1/edit
   def edit
   end
 
-  # POST /testimonials
-  # POST /testimonials.json
   def create
     @testimonial = Testimonial.new(testimonial_params)
 
-    respond_to do |format|
-      if @testimonial.save
-        format.html { redirect_to @testimonial, notice: 'Testimonial was successfully created.' }
-        format.json { render :show, status: :created, location: @testimonial }
-      else
-        format.html { render :new }
-        format.json { render json: @testimonial.errors, status: :unprocessable_entity }
-      end
+    if @testimonial.save
+      redirect_to @testimonial, notice: 'Testimonial was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /testimonials/1
-  # PATCH/PUT /testimonials/1.json
   def update
-    respond_to do |format|
-      if @testimonial.update(testimonial_params)
-        format.html { redirect_to @testimonial, notice: 'Testimonial was successfully updated.' }
-        format.json { render :show, status: :ok, location: @testimonial }
-      else
-        format.html { render :edit }
-        format.json { render json: @testimonial.errors, status: :unprocessable_entity }
-      end
+    if @testimonial.update(testimonial_params)
+      redirect_to @testimonial, notice: 'Testimonial was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /testimonials/1
-  # DELETE /testimonials/1.json
   def destroy
     @testimonial.destroy
-    respond_to do |format|
-      format.html { redirect_to testimonials_url, notice: 'Testimonial was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to testimonials_url
   end
 
   private
