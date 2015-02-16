c749eae1fb3065e83ade78ccb09832875d7a51b5
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-02-16 02:20:38 +0100

    couple of small changes, including writing, and then commenting out, a couple of duff methods in the testimonials controller that I could do with resolving - some problem with the use of the params

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -1,5 +1,6 @@
 class TestimonialsController < ApplicationController
   before_action :set_testimonial, only: [:show, :edit, :update, :destroy]
+  # before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :require_login, except: [:index, :show]
 
   def index
@@ -43,6 +44,20 @@ class TestimonialsController < ApplicationController
     def set_testimonial
       @testimonial = Testimonial.find(params[:id])
     end
+
+    # Attempted method to prevent access to editing testimonials that aren't the user's.
+    # However, the 'params' causes a problem (apparent when trying it in the console)
+    # def correct_user
+    #   @testimonial = current_user.testimonials.find(params[:id])
+    #   redirect_to root_path if @testimonial.nil?
+    # end
+
+    # Corrected method - 'find_by' (and adding :id) doesn't raise an error. Would be good to find out why at some point (for my own learning)
+    # EDIT - doesn't work either!
+    # def correct_user
+    #   @testimonial = current_user.testimonials.find_by params[:id]
+    #   redirect_to root_path if @testimonial.nil?
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def testimonial_params
